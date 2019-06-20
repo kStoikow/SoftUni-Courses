7e631c439d7fc7feaced355ea1141556945be358
kStoikow <kristiqn.stoikow11@gmail.com>
2019-06-20 16:26:42 +0300

    Reworked

--- Technology Fundamentals with JS/Lab/Arrays/01-sumFirstAndLastArrayElements.js
+++
@@ -1,7 +0,0 @@
-function sumOfFirstAndLastNum(arrayOfNumbers) {
-    let sum = Number(arrayOfNumbers[0]) + Number(arrayOfNumbers[arrayOfNumbers.length - 1]);
-    console.log(sum);
-
-
-
-} 
--- Technology Fundamentals with JS/Lab/Arrays/04-reverseAnArrayOfStrings.js
+++
@@ -1,8 +0,0 @@
-function reverseArr (arr){
-    for (let i = 0; i < arr.length / 2 ; i++) {
-        let currentElement = arr[i];
-        arr[i] = arr[arr.length - 1 -i];
-        arr[arr.length - 1 -i] = currentElement;
-    }
-    console.log(arr.join(' '));
-}
